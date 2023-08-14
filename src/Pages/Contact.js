89108b8f4033f9bf3961de4499b8e431ff93b4c4
ph03n1xc0d3s <ph03n1xc0d3s@gmail.com>
2023-08-14 17:42:29 +0530

    minor changes in UI

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "./Layout";
 import Shoe from "../assets/images/hero-image.png";
+import '../assets/css/Contact.css'
 
 const Contact = () => {
   return (
@@ -8,7 +9,8 @@ const Contact = () => {
       <div className="mainContainer">
         <div className="textContainer">
           <div className="text">
-            <h2>Step into Support: Connecting Your Stride with Our Service</h2>
+            <h2 className="heading">Step into Support: Connecting Your Stride with Our Service</h2>
+            <p className="tagline">At Nike, we value your feedback, inquiries, and suggestions. Our dedicated customer support team is here to assist you with any questions or concerns you might have. Please feel free to reach out to us through the following contact options</p>
           </div>
         </div>
         <div className="imageContainer">
