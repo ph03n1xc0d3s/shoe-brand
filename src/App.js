76a60e13f78eac3b01ac09ad4b73352377311809
ph03n1xc0d3s <ph03n1xc0d3s@gmail.com>
2023-08-14 17:52:08 +0530

    added career and about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import Home from "./Pages/Home";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from "./Pages/Contact";
+import About from "./Pages/About";
+import Career from "./Pages/Career";
 
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/contact" element={<Contact/>}/>
-      <Route path="/about" />
+      <Route path="/about" element={<About />}/>
+      <Route path="/career" element={<Career />}/>
     </Routes>
     </Router>
     </div>
---
+++ src/Pages/Career.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './Layout'
+
+const Career = () => {
+  return (
+    <Layout>
+      <h2>Underwork</h2>
+    </Layout>
+  )
+}
+
+export default Career
